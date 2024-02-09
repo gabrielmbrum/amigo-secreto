dd3fa13deef38c7272ea69fd819ff2cdc455585f
Brum <gabrielbrum2805@gmail.com>
2024-02-09 11:20:37 -0300

    adding new usability

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let friends = [];
 
+//returns a random integer number 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -8,8 +9,11 @@ function adicionar ()
 {
     let friend = document.getElementById('nome-amigo');
     let list = document.getElementById('lista-amigos');
+
+    //add the friend to the list of friends
     friends.push(friend.value);
 
+    //if is empty, else...
     if (list.textContent == '')
     {
         list.textContent = friend.value;
@@ -19,6 +23,34 @@ function adicionar ()
     }
 
     friend.value = null;
+
+    atualizarLista();
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+
+    for (let i = 0; i < friends.length; i++) {
+        //create a paragraph element for each friend
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = friends[i];
+       
+        //add a click event to remove the friend clicked
+        paragrafo.addEventListener('click', function() {
+            excluirAmigo(i);
+        });
+
+
+        //add the paragraph to the list
+        lista.appendChild(paragrafo);
+    }
+}
+
+function excluirAmigo(index) {
+    friends.splice(index, 1);
+    atualizarLista();
 }
 
 function embaralha(lista) {
@@ -27,7 +59,7 @@ function embaralha(lista) {
 
         const indiceAleatorio = Math.floor(Math.random() * indice);
 
-        // atribuição via destructuring
+        //attribution by destructuring
         [lista[indice - 1], lista[indiceAleatorio]] = 
             [lista[indiceAleatorio], lista[indice - 1]];
     }
@@ -36,10 +68,13 @@ function embaralha(lista) {
 function sortear ()
 {
     if (friends[0] == null)
-        alert('the friends list is empty');
+        alert('the friends list is empty!!!');
     else
     {
+        //sort and increment the friends that picked each other
+
         let sortList = document.getElementById('lista-sorteio');
+        sortList.innerHTML = '';
         embaralha(friends);
         let aux;
         for (aux = 0; aux < friends.length - 1; aux++)
@@ -54,6 +89,7 @@ function sortear ()
 
 function reiniciar ()
 {
+    //reset all the elements
     let list = document.getElementById('lista-amigos');
     let sortList = document.getElementById('lista-sorteio');
 
